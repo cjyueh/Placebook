bf4928cc712bc791388fdd8ed747059a4bb05b5c
cjyueh <cjyueh@gmail.com>
2016-03-26 23:45:28 -0700

    creates users model

---
+++ app/models/model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username: {type: String, required: true},
+    hometown: {type: String, required: true},
+    location: {type: [Number], required: true}, // [Long, Lat]
+    htmlverified: String,
+    created_at: {type: Date, default: Date.now()},
+    updated_at: {type: Date, default: Date.now()}
+});
+
+//indexes this schema in 2dsphere format (critical for running proximity searches)
+UserSchema.index({location: '2dsphere'});
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
